0ca76eee52c7796df35ea7db650269ccb0cd5b7b
Wahib Farhat <wahibfarhat13@gmail.com>
2021-04-30 18:44:16 +0000

    models and migrations created. admin updated.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import *
+admin.site.register(Pizza)
+admin.site.register(Customer)
+admin.site.register(Order)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=12)
+    address = models.TextField()
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, models.SET_NULL, null=True, blank=True)
+    RECEIVED = 'RE'
+    DISPATCHED = 'DI'
+    DELIVERED = 'DE'
+    STATUS_CHOICES = [
+        (RECEIVED, 'RECEIVED'),
+        (DISPATCHED, 'DISPATCHED'),
+        (DELIVERED, 'DELIVERED'),
+    ]
+    status = models.CharField(choices=STATUS_CHOICES, max_length=2)
+
+
+class Pizza(models.Model):
+    MARGARITA = 'MG'
+    MARINARA = 'MA'
+    SALAMI = 'SA'
+    FLAVOUR_CHOICES = [
+        (MARGARITA, 'margarita'),
+        (MARINARA, 'marinara'),
+        (SALAMI, 'salami'),
+    ]
+    SMALL = 'S'
+    MEDIUM = 'M'
+    LARGE = 'L'
+    SIZE_CHOICES = [
+        (SMALL, 'SMALL'),
+        (MEDIUM, 'MEDIUM'),
+        (LARGE, 'LARGE'),
+    ]
+    flavour = models.CharField(choices=FLAVOUR_CHOICES, max_length=2)
+    size = models.CharField(choices=SIZE_CHOICES, max_length=2)
+
+    order = models.ForeignKey(Order, models.CASCADE)
